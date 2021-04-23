e3d34d1cbff843f5306e3ee6bb6e6ab7cd9b9e1d
maciacco <mariociacco97@gmail.com>
2021-04-23 17:55:41 +0200

    fix bug in rec counting (particle with only e+/e- as daughters is not absorbed) + fix bug in gen counting + add MB analysis

--- Hypertriton_PbPb/He3AbsAnalysisPt.cpp
+++ Hypertriton_PbPb/He3AbsAnalysisPt.cpp
@@ -7,6 +7,8 @@
 #include "AliMCEventHandler.h"
 #endif
 
+# define DEBUG_ABSORPTION
+
 double kHe3Mass = 2.809230089;
 
 constexpr double Sq(double x) { return x * x; }
@@ -28,40 +30,94 @@ double ComputeHe3Ct(AliVParticle *he3Part, AliVParticle *dauPart)
 void He3AbsAnalysisPt(std::string pathToSimulation = "./")
 {
     int nCent = 3;
+    int nCentMB = 3;
     double centClasses[][2] = {{0, 5}, {5, 10}, {30, 50}};
-    int centIndex[3] = {0, 1, 3};
-    int nFun = 1;
-    TF1* fun[3][5];
-    std::string names[5]{"BGBW","Boltzmann","Mt-exp","Pt-exp","LevyTsallis"};
-    std::string antimattMatt[2]{"anti","matt"};
-    TFile inputFun("Anti_fits.root");
+    int centIndex[3] = {1, 2, 4};
 
+    int nFun = 1;
+    int nFunMB = 1;
+
+    TF1* fun[3][5];
+    TF1* funMB[3][4];
+
+    std::string names[5]{"BGBW","Boltzmann","Mt-exp","Pt-exp","LevyTsallis"};
+    std::string namesMB[4]{"BlastWave","Boltzmann","LevyTsallis","Mt-exp"};
+    std::string antimattMatt[2]{"anti","matt"};
+
+    TFile inputFun("Anti_fits.root");
+    TFile inputFunMB("BlastWaveFits.root");
+    TFile inputCent("AnalysisResults_18.root"); // centrality distribution (for MB analysis)
+
+    // get functions/histograms from files
+    // analysis in centrality classes
     for (int iCent{0}; iCent < nCent; ++iCent) {
         for (int iFun{0}; iFun < nFun; ++iFun) {
             fun[iCent][iFun] = (TF1*)inputFun.Get(Form("%s/%d/%s%d",names[iFun].c_str(),centIndex[iCent],names[iFun].c_str(),centIndex[iCent]));
         }
     }
+    // MB analysis
+    for (int iCent{0}; iCent < 3; ++iCent) {
+        for (int iFun{0}; iFun < nFunMB; ++iFun) {
+            funMB[iCent][iFun] = (TF1*)inputFunMB.Get(Form("%s/%s%d",namesMB[iFun].c_str(),namesMB[iFun].c_str(),iCent));
+        }
+    }
+    TH1D *centHist = (TH1D*)inputCent.Get("Centrality_selected");
 
-    TH1D *recPt[3][2][4], *recCt[3][2][4], *genPt[3][2][4], *genCt[3][2][4];
-    double maxPt[3][4];
+    // declare histograms
+    TH1D *recPt[3][2][5], *recCt[3][2][5], *genPt[3][2][5], *genCt[3][2][5];
+    TH2F *recPtCt[3][2][5], *genPtCt[3][2][5];
+    TH1D *recPtNotAbsorbed[3][2][5], *nDaughters[3][2][5], *daughterPDGCode[3][2][5];
+    TH1D *recInt[3][2][5], *genInt[3][2][5];                             // pt and ct integrated analysis
+    TH1D *recPtMB[2][4], *recCtMB[2][4], *genPtMB[2][4], *genCtMB[2][4]; // MB analysis
 
-    double ctBins[] = {2, 4, 8, 14, 35};
+    double maxPt[3][5];
+    double maxPtMB[3][4];
+    double maxEv; // event centrality distribution from data
 
+    double ctBins[] = {0, 2, 4, 8, 14, 35};
+    double ptBins[51];
+
+    for (int i=0; i<51; ++i) ptBins[i] = 0.2*i;
+
+    // book histograms
+    // analysis in centrality classes
     for (int iCent{0}; iCent < nCent; ++iCent) {
         for (int iFun{0}; iFun < nFun; ++iFun) {
             for (int iMatt{0}; iMatt < 2; ++iMatt) {
                 recPt[iCent][iMatt][iFun] = new TH1D(Form("recPt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); Counts", 50, 0, 10);
-                recCt[iCent][iMatt][iFun] = new TH1D(Form("recCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{c}t (cm); Counts", 4, ctBins);
+                recCt[iCent][iMatt][iFun] = new TH1D(Form("recCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{c}t (cm); Counts", 5, ctBins);
                 genPt[iCent][iMatt][iFun] = new TH1D(Form("genPt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); Counts", 50, 0, 10);
-                genCt[iCent][iMatt][iFun] = new TH1D(Form("genCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{c}t (cm);Counts;", 4, ctBins);
+                genCt[iCent][iMatt][iFun] = new TH1D(Form("genCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{c}t (cm);Counts;", 5, ctBins);
+
+                recInt[iCent][iMatt][iFun] = new TH1D(Form("recInt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{c}t (cm);Counts;", 1, 0, 35);
+                genInt[iCent][iMatt][iFun] = new TH1D(Form("genInt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{c}t (cm);Counts;", 1, 0, 35);
+
+                recPtCt[iCent][iMatt][iFun] = new TH2F(Form("recPtCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); #it{c}t (cm);", 50, ptBins, 5, ctBins);
+                genPtCt[iCent][iMatt][iFun] = new TH2F(Form("genPtCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); #it{c}t (cm);", 50, ptBins, 5, ctBins);
+
+                recPtNotAbsorbed[iCent][iMatt][iFun] = new TH1D(Form("recPtNotAbsorbed_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); Counts", 50, 0, 10);
+                nDaughters[iCent][iMatt][iFun] = new TH1D(Form("nDaughters_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; nDaughters; Counts;", 1001, 0, 1000);
+                daughterPDGCode[iCent][iMatt][iFun] = new TH1D(Form("daughterPDGCode_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()), "; nDaughters; Counts;", 100, 0, 99);
             }
             maxPt[iCent][iFun] = fun[iCent][iFun]->GetMaximum();
+        }
+    }
+    // MB analysis
+    for (int iFun{0}; iFun < nFunMB; ++iFun) {
+        for (int iMatt{0}; iMatt < 2; ++iMatt) {
+            recPtMB[iMatt][iFun] = new TH1D(Form("recPtMB_%s_%s",antimattMatt[iMatt].data(),namesMB[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); Counts", 50, 0, 10);
+            recCtMB[iMatt][iFun] = new TH1D(Form("recCtMB_%s_%s",antimattMatt[iMatt].data(),namesMB[iFun].data()), "; #it{c}t (cm);Counts;", 5, ctBins);
+            genPtMB[iMatt][iFun] = new TH1D(Form("genPtMB_%s_%s",antimattMatt[iMatt].data(),namesMB[iFun].data()), "; #it{p}_{T} (GeV/#it{c}); Counts", 50, 0, 10);
+            genCtMB[iMatt][iFun] = new TH1D(Form("genCtMB_%s_%s",antimattMatt[iMatt].data(),namesMB[iFun].data()), "; #it{c}t (cm);Counts;", 5, ctBins);
+        }
+        for (int iCent{0}; iCent < nCentMB; ++iCent) {
+            maxPtMB[iCent][iFun] = funMB[iCent][iFun]->GetMaximum();
         }
     }
 
     int kNorm = 0;
     int dirs[] = {295585, 295586};
-    int subdirs[] = {38, 160};
+    int subdirs[] = {38, 1};
 
     for (Int_t iDir = 0; iDir <= 1; iDir++)
     {
@@ -82,10 +138,14 @@ void He3AbsAnalysisPt(std::string pathToSimulation = "./")
                 for (Int_t i = 0; i < mcEv->GetNumberOfTracks(); ++i)
                 {
                     AliVParticle *part = mcEv->GetTrack(i);
+                    // if (part->Pt() < 2) continue; // pt cut
+
                     if (part->IsPhysicalPrimary() && std::abs(part->PdgCode()) == 1000020030)
                     {
-                        if (part->Pt() < 2) continue; // pt cut
+                        int isMatter = 0;
+                        if (part->PdgCode() > 0) isMatter = 1;
 
+                        // analysis performed in centrality classes
                         for (int iCent{0}; iCent < nCent; ++iCent) {
                             for (int iFun{0}; iFun < nFun; ++iFun) {
                                 float hypPtShapeNum = fun[iCent][iFun]->Eval(part->Pt());
@@ -94,29 +154,104 @@ void He3AbsAnalysisPt(std::string pathToSimulation = "./")
                                 }
                                 kNorm++;
 
-                                int isMatter = 0;
-                                if (part->PdgCode() > 0) isMatter = 1;
-
+                                double decCt = gRandom->Exp(7.6);
                                 genPt[iCent][isMatter][iFun]->Fill(part->Pt());
-                                for (int c = part->GetDaughterFirst(); c < part->GetDaughterLast(); c++)
+                                genCt[iCent][isMatter][iFun]->Fill(decCt);
+                                genInt[iCent][isMatter][iFun]->Fill(decCt);
+                                genPtCt[iCent][isMatter][iFun]->Fill(part->Pt(),decCt);
+                                int checkedDaughters=0;
+                                if(part->GetNDaughters()>0){
+                                    for (int c = part->GetDaughterFirst(); c <= part->GetDaughterLast(); ++c)
+                                    {
+                                        AliVParticle *dPart = mcEv->GetTrack(c);
+                                        int dPartPDG = std::abs(dPart->PdgCode());
+                                        if (dPartPDG != 22 && dPartPDG != 11)
+                                        {
+                                            double absCt = ComputeHe3Ct(part, dPart);
+                                            bool isAbsorbed = absCt < decCt;
+                                            if (!isAbsorbed) {
+                                                recPt[iCent][isMatter][iFun]->Fill(part->Pt());
+                                                recCt[iCent][isMatter][iFun]->Fill(decCt);
+                                                recInt[iCent][isMatter][iFun]->Fill(decCt);
+                                                recPtCt[iCent][isMatter][iFun]->Fill(part->Pt(),decCt);
+                                            }
+                                            break;
+                                        }
+                                        checkedDaughters++;
+                                    }
+                                }
+
+                                // not-absorbed particles
+                                if (checkedDaughters==part->GetNDaughters()) {
+                                    recPt[iCent][isMatter][iFun]->Fill(part->Pt());
+                                    recCt[iCent][isMatter][iFun]->Fill(decCt);
+                                    recInt[iCent][isMatter][iFun]->Fill(decCt);
+                                    recPtCt[iCent][isMatter][iFun]->Fill(part->Pt(),decCt);
+
+                                    #ifdef DEBUG_ABSORPTION
+                                        nDaughters[iCent][isMatter][iFun]->Fill(part->GetNDaughters());
+                                        if(part->GetNDaughters()>0){
+                                            for (int c = part->GetDaughterFirst(); c <= part->GetDaughterLast(); ++c)
+                                            {
+                                                AliVParticle *dPart = mcEv->GetTrack(c);
+                                                int dPartPDG = std::abs(dPart->PdgCode());
+                                                daughterPDGCode[iCent][isMatter][iFun]->Fill(dPartPDG);
+                                            }
+                                        }
+                                        recPtNotAbsorbed[iCent][isMatter][iFun]->Fill(part->Pt());
+                                    #endif // DEBUG_ABSORPTION
+                                }
+                            }
+                        }
+
+                        // MB analysis
+                        for (int iFun{0}; iFun < nFunMB; ++iFun) {
+                            // sample centrality
+                            float cent = (gRandom->Rndm() * 90.);
+                            float evFromUniformCent = centHist->GetBinContent(centHist->FindBin(cent));
+                            if (gRandom->Rndm() * maxEv > evFromUniformCent) {
+                                continue;
+                            }
+                            int iCent = 0;
+                            if (cent > 10 && cent < 40) iCent = 1;
+                            else if (cent > 40) iCent = 2; 
+
+                            float hypPtShapeNum = funMB[iCent][iFun]->Eval(part->Pt());
+                            if (gRandom->Rndm() * maxPtMB[iCent][iFun] > hypPtShapeNum) {
+                                continue;
+                            }
+                            kNorm++;
+
+                            double decCt = gRandom->Exp(7.6);
+                            genPtMB[isMatter][iFun]->Fill(part->Pt());
+                            genCtMB[isMatter][iFun]->Fill(decCt);
+                            int checkedDaughters=0;
+                            if(part->GetNDaughters()>0){
+                                for (int c = part->GetDaughterFirst(); c <= part->GetDaughterLast(); ++c)
                                 {
                                     AliVParticle *dPart = mcEv->GetTrack(c);
                                     int dPartPDG = std::abs(dPart->PdgCode());
                                     if (dPartPDG != 22 && dPartPDG != 11)
                                     {
                                         double absCt = ComputeHe3Ct(part, dPart);
-                                        double decCt = gRandom->Exp(7.6);
-                                        genCt[iCent][isMatter][iFun]->Fill(decCt);
                                         bool isAbsorbed = absCt < decCt;
                                         if (!isAbsorbed) {
-                                            recPt[iCent][isMatter][iFun]->Fill(part->Pt());
-                                            recCt[iCent][isMatter][iFun]->Fill(decCt);
+                                            recPtMB[isMatter][iFun]->Fill(part->Pt());
+                                            recCtMB[isMatter][iFun]->Fill(decCt);
                                         }
                                         break;
                                     }
+                                    checkedDaughters++;
                                 }
                             }
+
+                            // not-absorbed particles
+                            if (checkedDaughters==part->GetNDaughters()) {
+                                recPtMB[isMatter][iFun]->Fill(part->Pt());
+                                recCtMB[isMatter][iFun]->Fill(decCt);
+                            }
                         }
+
                     }
                 }
             }
@@ -139,8 +274,36 @@ void He3AbsAnalysisPt(std::string pathToSimulation = "./")
                 effPt.Write(Form("effPt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()));
                 TGraphAsymmErrors effCt(recCt[iCent][iMatt][iFun],genCt[iCent][iMatt][iFun]);
                 effCt.Write(Form("effCt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()));
+
+                recPtCt[iCent][iMatt][iFun]->Write();
+                recPtNotAbsorbed[iCent][iMatt][iFun]->Write();
+                genPtCt[iCent][iMatt][iFun]->Write();
+
+                recInt[iCent][iMatt][iFun]->Write();
+                genInt[iCent][iMatt][iFun]->Write();
+                TGraphAsymmErrors effInt(recInt[iCent][iMatt][iFun],genInt[iCent][iMatt][iFun]);
+                effInt.Write(Form("effInt_%.0f_%.0f_%s_%s",centClasses[iCent][0],centClasses[iCent][1],antimattMatt[iMatt].data(),names[iFun].data()));
+
+                nDaughters[iCent][iMatt][iFun]->Write();
+                daughterPDGCode[iCent][iMatt][iFun]->Write();
             }
         }
     }
+
+    fFile.mkdir("MB");
+    fFile.cd("MB");
+    for (int iMatt{0}; iMatt < 2; ++iMatt) {
+        for (int iFun{0}; iFun < nFunMB; ++iFun) {
+            recPtMB[iMatt][iFun]->Write();
+            recCtMB[iMatt][iFun]->Write();
+            genPtMB[iMatt][iFun]->Write();
+            genCtMB[iMatt][iFun]->Write();
+            TGraphAsymmErrors effPtMB(recPtMB[iMatt][iFun],genPtMB[iMatt][iFun]);
+            effPtMB.Write(Form("effPtMB_%s_%s",antimattMatt[iMatt].data(),namesMB[iFun].data()));
+            TGraphAsymmErrors effCtMB(recCtMB[iMatt][iFun],genCtMB[iMatt][iFun]);
+            effCtMB.Write(Form("effCtMB_%s_%s",antimattMatt[iMatt].data(),namesMB[iFun].data()));
+        }
+    }
+
     fFile.Close();
 }
