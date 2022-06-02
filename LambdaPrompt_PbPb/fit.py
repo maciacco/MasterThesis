affcde7a57c9c9745736a80eecdbf7ae376d9592
maciacco <mariociacco97@gmail.com>
2022-06-02 18:39:07 +0200

    proton its calibration + test lambda fit

--- LambdaPrompt_PbPb/fit.py
+++ LambdaPrompt_PbPb/fit.py
@@ -48,7 +48,7 @@ SPLIT_LIST = ['all']
 if SPLIT:
     SPLIT_LIST = ['antimatter', 'matter']
 
-f = ROOT.TFile("out.root","recreate")
+f = ROOT.TFile("out_1.root","recreate")
 
 score_eff_dict = pickle.load(open('file_score_eff_dict','rb'))
 
@@ -61,121 +61,121 @@ for split in SPLIT_LIST:
 
     for i_cent_bins in range(len(CENTRALITY_LIST)):
         cent_bins = CENTRALITY_LIST[i_cent_bins]
-        h = ROOT.TH1D("h","h",80,0,40)
         for ct_bins in zip(CT_BINS_CENT[i_cent_bins][:-1], CT_BINS_CENT[i_cent_bins][1:]):
-            if ct_bins[0] < 5 or ct_bins[1] > 10:
+            if ct_bins[0] < 5 or ct_bins[1] > 5.5:
                 continue
-            bdt_cut = 0.05
-            bin = f'{split}_{cent_bins[0]}_{cent_bins[1]}_{ct_bins[0]}_{ct_bins[1]}'
-            bin_mc = f'test_data_{cent_bins[0]}_{cent_bins[1]}_{ct_bins[0]}_{ct_bins[1]}'
-            df_data = uproot.open(os.path.expandvars("../data/LambdaPrompt_PbPb/outTree.root"))['LambdaTreeBDTOut'].arrays(library="pd")
-            df_mc = pd.read_parquet(f'df/df_alidbz/df/{bin_mc}.parquet.gzip')
+            h_raw_yields = ROOT.TH1D("fRawYields","fRawYields",100,0,100)
+            for bdt_eff in range(61,82):
+                bdt_cut = 1-bdt_eff/100.
+                bin = f'{split}_{cent_bins[0]}_{cent_bins[1]}_{ct_bins[0]}_{ct_bins[1]}'
+                bin_mc = f'test_data_{cent_bins[0]}_{cent_bins[1]}_{ct_bins[0]}_{ct_bins[1]}'
+                df_data = uproot.open(os.path.expandvars("../data/LambdaPrompt_PbPb/outTree.root"))['LambdaTreeBDTOut'].arrays(library="pd")
+                df_mc = pd.read_parquet(f'df/df_alidbz/df/{bin_mc}.parquet.gzip')
 
-            # apply cut
-            df_data_cut = df_data.query(f"bdtOutputBackground < {bdt_cut} and ct > {ct_bins[0]} and ct < {ct_bins[1]} and mass > 1.09 and mass < 1.15")
-            df_mc_cut = df_mc.query(f"model_output_background < {bdt_cut} and mass > 1.09 and mass < 1.15")
+                # apply cut
+                df_data_cut = df_data.query(f"bdtOutputPrompt > {bdt_cut} and ct > {ct_bins[0]} and ct < {ct_bins[1]} and mass > 1.09 and mass < 1.15")
+                df_mc_cut = df_mc.query(f"model_output_prompt > {bdt_cut} and mass > 1.09 and mass < 1.15")
 
-            # get invariant mass
-            data_inv_mass = df_data_cut["mass"]
-            mc_inv_mass_bkg = df_mc_cut.query("y_true==0")["mass"]
-            mc_inv_mass_sig = df_mc_cut.query("y_true==2 or y_true==1")["mass"]
+                # get invariant mass
+                data_inv_mass = df_data_cut["mass"]
+                mc_inv_mass_bkg = df_mc_cut.query("y_true==0")["mass"]
+                mc_inv_mass_sig = df_mc_cut.query("y_true==2 or y_true==1")["mass"]
 
-            plt.hist(data_inv_mass, bins=1000)
-            plt.savefig("plt.pdf")
+                plt.hist(data_inv_mass, bins=1000)
+                plt.savefig("plt.pdf")
 
-            # get non-prompt bdt score
-            data_bdt_non_prompt_out = df_data_cut["bdtOutputNonPrompt"]
-            mc_prompt_bdt_non_prompt_out = df_mc_cut.query("y_true==2")["model_output_non_prompt"]
-            mc_non_prompt_bdt_non_prompt_out = df_mc_cut.query("y_true==1")["model_output_non_prompt"]
-            mc_background_bdt_non_prompt_out = df_mc_cut.query("y_true==0")["model_output_non_prompt"]
+                # get non-prompt bdt score
+                data_bdt_non_prompt_out = df_data_cut["bdtOutputNonPrompt"]
+                mc_prompt_bdt_non_prompt_out = df_mc_cut.query("y_true==2")["model_output_non_prompt"]
+                mc_non_prompt_bdt_non_prompt_out = df_mc_cut.query("y_true==1")["model_output_non_prompt"]
+                mc_background_bdt_non_prompt_out = df_mc_cut.query("y_true==0")["model_output_non_prompt"]
 
-            mc_count_p = mc_prompt_bdt_non_prompt_out.count()
-            mc_count_np = mc_non_prompt_bdt_non_prompt_out.count()
+                mc_count_p = mc_prompt_bdt_non_prompt_out.count()
+                mc_count_np = mc_non_prompt_bdt_non_prompt_out.count()
 
-            # fit to invariant mass
-            inv_mass = ROOT.RooRealVar("m","m",1.09,1.15)
-            inv_mass.setBins(200)
-            inv_mass_roo_data = helpers.ndarray2roo(data_inv_mass.to_numpy(),inv_mass)
-            inv_mass_data = ROOT.RooDataHist("db_m","db_m",ROOT.RooArgList(inv_mass),inv_mass_roo_data)
-            inv_mass_roo_mc = helpers.ndarray2roo(mc_inv_mass_sig.to_numpy(),inv_mass)
-            inv_mass_mc = ROOT.RooDataHist("db_m_mc","db_m_mc",ROOT.RooArgList(inv_mass),inv_mass_roo_mc)
-            mass = ROOT.RooRealVar('mass','mass',1.112,1.118)
-            sigma = ROOT.RooRealVar('sigma','sigma',0.,0.005)
-            # alpha_left = ROOT.RooRealVar('alpha_left','alpha_left',0.,2.)
-            # alpha_right = ROOT.RooRealVar('alpha_right','alpha_right',0.,2.)
-            # n_left = ROOT.RooRealVar('n_left','n_left',0.,15.)
-            # n_right = ROOT.RooRealVar('n_right','n_right',0.,15.)
-            par_a = ROOT.RooRealVar('a','a',-10.,10.)
-            par_b = ROOT.RooRealVar('b','b',-10.,10.)
-            signal_pdf = ROOT.RooGaussian('signal','signal',inv_mass,mass,sigma)
-            # signal_pdf = ROOT.RooDSCBShape('signal','signal',inv_mass,mass,sigma,alpha_left,n_left,alpha_right,n_right)
-            signal_pdf.fitTo(inv_mass_mc)
-            sigma.setConstant()
-            # mass.setConstant()
-            # alpha_left.setConstant()
-            # alpha_right.setConstant()
-            # n_left.setConstant()
-            # n_right.setConstant()
-            bkg_pdf = ROOT.RooPolynomial('bkg','bkg',inv_mass,ROOT.RooArgList(par_a,par_b))
-            w_signal = ROOT.RooRealVar("w_signal","w_signal",0.,1.)
-            n_tot = ROOT.RooRealVar("n_tot","n_tot",0.,5.e2)
-            model_mass_ = ROOT.RooAddPdf("model_mass_","model_mass_",signal_pdf,bkg_pdf,w_signal)
-            model_mass = ROOT.RooAddPdf("model_mass","model_mass",ROOT.RooArgList(model_mass_),ROOT.RooArgList(n_tot))
+                # fit to invariant mass
+                inv_mass = ROOT.RooRealVar("m","m",1.09,1.15)
+                inv_mass.setBins(200)
+                inv_mass_roo_data = helpers.ndarray2roo(data_inv_mass.to_numpy(),inv_mass)
+                inv_mass_data = ROOT.RooDataHist("db_m","db_m",ROOT.RooArgList(inv_mass),inv_mass_roo_data)
+                inv_mass_roo_mc = helpers.ndarray2roo(mc_inv_mass_sig.to_numpy(),inv_mass)
+                inv_mass_mc = ROOT.RooDataHist("db_m_mc","db_m_mc",ROOT.RooArgList(inv_mass),inv_mass_roo_mc)
+                mass = ROOT.RooRealVar('mass','mass',1.112,1.118)
+                sigma = ROOT.RooRealVar('sigma','sigma',0.,0.005)
+                # alpha_left = ROOT.RooRealVar('alpha_left','alpha_left',0.,2.)
+                # alpha_right = ROOT.RooRealVar('alpha_right','alpha_right',0.,2.)
+                # n_left = ROOT.RooRealVar('n_left','n_left',0.,15.)
+                # n_right = ROOT.RooRealVar('n_right','n_right',0.,15.)
+                par_a = ROOT.RooRealVar('a','a',-10.,10.)
+                par_b = ROOT.RooRealVar('b','b',-10.,10.)
+                signal_pdf = ROOT.RooGaussian('signal','signal',inv_mass,mass,sigma)
+                # signal_pdf = ROOT.RooDSCBShape('signal','signal',inv_mass,mass,sigma,alpha_left,n_left,alpha_right,n_right)
+                signal_pdf.fitTo(inv_mass_mc)
+                sigma.setConstant()
+                # mass.setConstant()
+                # alpha_left.setConstant()
+                # alpha_right.setConstant()
+                # n_left.setConstant()
+                # n_right.setConstant()
+                bkg_pdf = ROOT.RooPolynomial('bkg','bkg',inv_mass,ROOT.RooArgList(par_a,par_b))
+                w_signal = ROOT.RooRealVar("w_signal","w_signal",0.,1.)
+                n_tot = ROOT.RooRealVar("n_tot","n_tot",0.,5.e2)
+                model_mass_ = ROOT.RooAddPdf("model_mass_","model_mass_",signal_pdf,bkg_pdf,w_signal)
+                model_mass = ROOT.RooAddPdf("model_mass","model_mass",ROOT.RooArgList(model_mass_),ROOT.RooArgList(n_tot))
 
-            # fit to bdt output
-            bdt_out = ROOT.RooRealVar("BDT out","BDT out",0.,1.)
-            bdt_out.setBins(100)
-            bdt_roo_data = helpers.ndarray2roo(data_bdt_non_prompt_out.to_numpy(), bdt_out)
-            bdt_data = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(bdt_out), bdt_roo_data)
-            bdt_roo_mc_prompt = helpers.ndarray2roo(mc_prompt_bdt_non_prompt_out.to_numpy(), bdt_out)
-            bdt_mc_prompt = ROOT.RooDataHist("dp", "dp", ROOT.RooArgList(bdt_out), bdt_roo_mc_prompt)
-            bdt_mc_prompt_pdf = ROOT.RooHistPdf("dppdf", "dppdf", ROOT.RooArgList(bdt_out), bdt_mc_prompt)
-            bdt_roo_mc_non_prompt = helpers.ndarray2roo(mc_non_prompt_bdt_non_prompt_out.to_numpy(), bdt_out)
-            bdt_mc_non_prompt = ROOT.RooDataHist("dnp", "dnp", ROOT.RooArgList(bdt_out), bdt_roo_mc_non_prompt)
-            bdt_mc_non_prompt_pdf = ROOT.RooHistPdf("dnppdf", "dnppdf", ROOT.RooArgList(bdt_out), bdt_mc_non_prompt)
-            bdt_roo_mc_bkg = helpers.ndarray2roo(mc_background_bdt_non_prompt_out.to_numpy(), bdt_out)
-            bdt_mc_bkg = ROOT.RooDataHist("db", "db", ROOT.RooArgList(bdt_out), bdt_roo_mc_bkg)
-            bdt_mc_bkg_pdf = ROOT.RooHistPdf("dbpdf", "dbpdf", ROOT.RooArgList(bdt_out), bdt_mc_bkg)
-            frame = bdt_out.frame(ROOT.RooFit.Name(f"fBDTOutNonPrompt_{ct_bins[0]}_{ct_bins[1]}"))
-            w_non_prompt = ROOT.RooRealVar("w_non_prompt","w_non_prompt",0.,1.)
-            model_signal = ROOT.RooAddPdf("model_signal","model_signal",bdt_mc_non_prompt_pdf,bdt_mc_prompt_pdf,w_non_prompt)
-            model = ROOT.RooAddPdf("model","model",model_signal,bdt_mc_bkg_pdf,w_signal)
+                # fit to bdt output
+                bdt_out = ROOT.RooRealVar("BDT out","BDT out",0.,1.)
+                bdt_out.setBins(100)
+                bdt_roo_data = helpers.ndarray2roo(data_bdt_non_prompt_out.to_numpy(), bdt_out)
+                bdt_data = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(bdt_out), bdt_roo_data)
+                bdt_roo_mc_prompt = helpers.ndarray2roo(mc_prompt_bdt_non_prompt_out.to_numpy(), bdt_out)
+                bdt_mc_prompt = ROOT.RooDataHist("dp", "dp", ROOT.RooArgList(bdt_out), bdt_roo_mc_prompt)
+                bdt_mc_prompt_pdf = ROOT.RooHistPdf("dppdf", "dppdf", ROOT.RooArgList(bdt_out), bdt_mc_prompt)
+                bdt_roo_mc_non_prompt = helpers.ndarray2roo(mc_non_prompt_bdt_non_prompt_out.to_numpy(), bdt_out)
+                bdt_mc_non_prompt = ROOT.RooDataHist("dnp", "dnp", ROOT.RooArgList(bdt_out), bdt_roo_mc_non_prompt)
+                bdt_mc_non_prompt_pdf = ROOT.RooHistPdf("dnppdf", "dnppdf", ROOT.RooArgList(bdt_out), bdt_mc_non_prompt)
+                bdt_roo_mc_bkg = helpers.ndarray2roo(mc_background_bdt_non_prompt_out.to_numpy(), bdt_out)
+                bdt_mc_bkg = ROOT.RooDataHist("db", "db", ROOT.RooArgList(bdt_out), bdt_roo_mc_bkg)
+                bdt_mc_bkg_pdf = ROOT.RooHistPdf("dbpdf", "dbpdf", ROOT.RooArgList(bdt_out), bdt_mc_bkg)
+                frame = bdt_out.frame(ROOT.RooFit.Name(f"fBDTOutNonPrompt_{ct_bins[0]}_{ct_bins[1]}"))
+                w_non_prompt = ROOT.RooRealVar("w_non_prompt","w_non_prompt",0.,1.)
+                model_signal = ROOT.RooAddPdf("model_signal","model_signal",bdt_mc_non_prompt_pdf,bdt_mc_prompt_pdf,w_non_prompt)
+                model = ROOT.RooAddPdf("model","model",model_signal,bdt_mc_bkg_pdf,w_signal)
 
-            # simultaneous fit
-            ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
-            ROOT.RooMsgService.instance().setSilentMode(ROOT.kTRUE)
-            ROOT.gErrorIgnoreLevel = ROOT.kError
-            sample = ROOT.RooCategory("sample","sample")
-            sample.defineType("bdt")
-            sample.defineType("mass")
-            combData = ROOT.RooDataHist("combData","combData",ROOT.RooArgSet(bdt_out,inv_mass),ROOT.RooFit.Index(sample),ROOT.RooFit.Import("bdt",bdt_data),ROOT.RooFit.Import("mass",inv_mass_data))
-            simPdf = ROOT.RooSimultaneous("simPdf","simPdf",sample)
-            simPdf.addPdf(model,"bdt")
-            simPdf.addPdf(model_mass,"mass")
-            simPdf.fitTo(combData)
+                # simultaneous fit
+                ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
+                ROOT.RooMsgService.instance().setSilentMode(ROOT.kTRUE)
+                ROOT.gErrorIgnoreLevel = ROOT.kError
+                sample = ROOT.RooCategory("sample","sample")
+                sample.defineType("bdt")
+                sample.defineType("mass")
+                combData = ROOT.RooDataHist("combData","combData",ROOT.RooArgSet(bdt_out,inv_mass),ROOT.RooFit.Index(sample),ROOT.RooFit.Import("bdt",bdt_data),ROOT.RooFit.Import("mass",inv_mass_data))
+                simPdf = ROOT.RooSimultaneous("simPdf","simPdf",sample)
+                simPdf.addPdf(model,"bdt")
+                simPdf.addPdf(model_mass,"mass")
+                simPdf.fitTo(combData)
 
-            # mass plot
-            frame_mass = inv_mass.frame(ROOT.RooFit.Name(f"fMass_{ct_bins[0]}_{ct_bins[1]}"))
-            inv_mass_data.plotOn(frame_mass)
-            model_mass.plotOn(frame_mass,ROOT.RooFit.Components("bkg"),ROOT.RooFit.Name('background'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kGreen))
-            model_mass.plotOn(frame_mass,ROOT.RooFit.Components("signal"),ROOT.RooFit.Name('signal'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
-            model_mass.plotOn(frame_mass)
+                # mass plot
+                frame_mass = inv_mass.frame(ROOT.RooFit.Name(f"fMass_{ct_bins[0]}_{ct_bins[1]}"))
+                inv_mass_data.plotOn(frame_mass)
+                model_mass.plotOn(frame_mass,ROOT.RooFit.Components("bkg"),ROOT.RooFit.Name('background'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kGreen))
+                model_mass.plotOn(frame_mass,ROOT.RooFit.Components("signal"),ROOT.RooFit.Name('signal'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
+                model_mass.plotOn(frame_mass)
 
-            # bdt plot
-            bdt_data.plotOn(frame)
-            model.plotOn(frame,ROOT.RooFit.Components('dbpdf'),ROOT.RooFit.Name('background'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kGreen))
-            model.plotOn(frame,ROOT.RooFit.Components('dnppdf'),ROOT.RooFit.Name('non-prompt'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kOrange))
-            model.plotOn(frame,ROOT.RooFit.Components('dppdf'),ROOT.RooFit.Name('prompt'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
-            model.plotOn(frame,ROOT.RooFit.Name('model'),ROOT.RooFit.LineColor(ROOT.kBlue))
+                # bdt plot
+                bdt_data.plotOn(frame)
+                model.plotOn(frame,ROOT.RooFit.Components('dbpdf'),ROOT.RooFit.Name('background'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kGreen))
+                model.plotOn(frame,ROOT.RooFit.Components('dnppdf'),ROOT.RooFit.Name('non-prompt'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kOrange))
+                model.plotOn(frame,ROOT.RooFit.Components('dppdf'),ROOT.RooFit.Name('prompt'),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
+                model.plotOn(frame,ROOT.RooFit.Name('model'),ROOT.RooFit.LineColor(ROOT.kBlue))
 
-            f.cd()
-            f.mkdir(f'{split}_{cent_bins[0]}_{cent_bins[0]}_{ct_bins[0]}_{ct_bins[1]}')
-            f.cd(f'{split}_{cent_bins[0]}_{cent_bins[0]}_{ct_bins[0]}_{ct_bins[1]}')
-            frame.Write()
-            frame_mass.Write()
+                f.cd()
+                f.mkdir(f'{split}_{cent_bins[0]}_{cent_bins[0]}_{ct_bins[0]}_{ct_bins[1]}')
+                f.cd(f'{split}_{cent_bins[0]}_{cent_bins[0]}_{ct_bins[0]}_{ct_bins[1]}')
+                frame.Write()
+                frame_mass.Write()
 
-            h.SetBinContent(h.FindBin(0.5*(ct_bins[0]+ct_bins[1])),n_tot.getVal()*w_signal.getVal()*(1-w_non_prompt.getVal()))
-            print(f'Non-prompt / prompt = {mc_count_np/(mc_count_p+mc_count_np)}')
+                h_raw_yields.SetBinContent(int(bdt_eff),n_tot.getVal()*w_signal.getVal()*(1-w_non_prompt.getVal()))
+                print(f'Non-prompt / prompt = {mc_count_np/(mc_count_p+mc_count_np)}')
 
-        f.cd()
-        h.Write()
+            h_raw_yields.Write()
--- Proton_PbPb/ITSrecalibration.cpp
+++ Proton_PbPb/ITSrecalibration.cpp
@@ -5,36 +5,82 @@
 #include <TH3F.h>
 #include <TH1D.h>
 #include <TF1.h>
+#include <RooRealVar.h>
+#include <RooPlot.h>
+#include <RooExponential.h>
+#include <RooAddPdf.h>
+#include <RooAbsPdf.h>
 
 #include "../utils/Config.h"
 #include "../utils/Utils.h"
+#include "../utils/RooGausExp.h"
 
 using namespace proton;
 using namespace utils;
 
 void ITSrecalibration(){
-  TFile *inFile=TFile::Open("../data/Proton_PbPb/AnalysisResults_ITS.root");
+  TFile *inFile=TFile::Open("../data/Proton_PbPb/AnalysisResults_.root");
   if (!inFile) std::cout << "Input file does not exist!" << std::endl;
   TFile outFile("out/ITScalib.root","recreate");
   TTList *ls_proton=(TTList*)inFile->Get("nuclei_proton_");
-  for (int iM=0;iM<2;++iM){
-    TH3F *fNSigmaITSpTCent=(TH3F*)ls_proton->Get(Form("f%sITSnSigma",kAntimatterMatter[iM]));
-    TH1D fITSCalibMean(Form("f%sITSCalibMean",kAntimatterMatter[iM]),";#it{p}_{T} (GeV/#it{c});Mean (a.u.)",kNPtBins,kPtBins);
-    TH1D fITSCalibSigma(Form("f%sITSCalibSigma",kAntimatterMatter[iM]),";#it{p}_{T} (GeV/#it{c});Sigma (a.u.)",kNPtBins,kPtBins);
-    outFile.mkdir(Form("fits_%s",kAntimatterMatter[iM]));
-    for (int iB=1;iB<kNPtBins+1;iB++){
-      outFile.cd(Form("fits_%s",kAntimatterMatter[iM]));
-      TH1D* fNSigmaITSpT=fNSigmaITSpTCent->ProjectionZ(Form("proj_%.2f_%.2f",kPtBins[iB-1],kPtBins[iB]),1,10,iB,iB);
-      fNSigmaITSpT->Fit("gaus","RQL","",-1.5,1.5);
-      if (fNSigmaITSpT->GetFunction("gaus")->GetChisquare()/fNSigmaITSpT->GetFunction("gaus")->GetNDF() > 3.) continue;
-      fITSCalibMean.SetBinContent(iB,fNSigmaITSpT->GetFunction("gaus")->GetParameter(1));
-      fITSCalibMean.SetBinError(iB,fNSigmaITSpT->GetFunction("gaus")->GetParError(1));
-      fITSCalibSigma.SetBinContent(iB,fNSigmaITSpT->GetFunction("gaus")->GetParameter(2));
-      fITSCalibSigma.SetBinError(iB,fNSigmaITSpT->GetFunction("gaus")->GetParError(2));
-      fNSigmaITSpT->Write();
-    }
-    outFile.cd("");
-    fITSCalibMean.Write();
-    fITSCalibSigma.Write();
+  TH3F *fNSigmaITSpTCentA=(TH3F*)ls_proton->Get(Form("f%sITSnSigma",kAntimatterMatter[0]));
+  TH3F *fNSigmaITSpTCentM=(TH3F*)ls_proton->Get(Form("f%sITSnSigma",kAntimatterMatter[1]));
+  TH1D fITSCalibMean("fITSCalibMean",";#it{p}_{T} (GeV/#it{c});Mean (a.u.)",kNPtBins,kPtBins);
+  TH1D fITSCalibSigma("fITSCalibSigma",";#it{p}_{T} (GeV/#it{c});Sigma (a.u.)",kNPtBins,kPtBins);
+  TH1D fITSCalibMeanRoo("fITSCalibMeanRoo",";#it{p}_{T} (GeV/#it{c});Mean (a.u.)",kNPtBins,kPtBins);
+  TH1D fITSCalibSigmaRoo("fITSCalibSigmaRoo",";#it{p}_{T} (GeV/#it{c});Sigma (a.u.)",kNPtBins,kPtBins);
+  outFile.mkdir("fits");
+  for (int iB=1;iB<kNPtBins+1;iB++){
+    if(kPtBins[iB-1]>0.89) continue;
+    outFile.cd("fits");
+    TH1D* fNSigmaITSpT=fNSigmaITSpTCentA->ProjectionZ(Form("proj_%.2f_%.2f",kPtBins[iB-1],kPtBins[iB]),1,10,iB,iB);
+    TH1D* fNSigmaITSpTM=fNSigmaITSpTCentM->ProjectionZ(Form("proj_%.2f_%.2f",kPtBins[iB-1],kPtBins[iB]),1,10,iB,iB);
+    fNSigmaITSpT->Add(fNSigmaITSpTM);
+    fNSigmaITSpT->Rebin(1);
+    double lowNsigma = -3.8;
+    if (kPtBins[iB-1]>0.39 && kPtBins[iB-1]<0.6) lowNsigma = -3.2;
+    else if (kPtBins[iB-1]>0.6) lowNsigma = -2.7;
+    RooRealVar nSigma("nSigma","nSigma",lowNsigma,4.);
+    RooDataHist data("data","data",RooArgList(nSigma),fNSigmaITSpT);
+    RooPlot *xframe = nSigma.frame();
+    RooRealVar mu("mu","mu",-0.5,-1.,1.);
+    RooRealVar sig("sig","sig",0.5,0.,1.);
+    RooRealVar tau("tau","tau",0.5,0.,2.);
+    RooRealVar slope("slope","slope",-10.,10.);
+    RooGausExp signal("signal","signal",nSigma,mu,sig,tau);
+    RooExponential bkg("bkg","bkg",nSigma,slope);
+    RooRealVar w("w","w",0.,1.);
+    RooAddPdf model("model","model",signal,bkg,w);
+    model.fitTo(data);
+    data.plotOn(xframe,RooFit::Name("data"));
+    model.plotOn(xframe,RooFit::Name("model"));
+    model.plotOn(xframe,RooFit::Components("signal"),RooFit::Name("signal"),RooFit::LineColor(kRed),RooFit::LineStyle(kDashed));
+    model.plotOn(xframe,RooFit::Components("bkg"),RooFit::Name("bkg"),RooFit::LineColor(kGreen),RooFit::LineStyle(kDashed));
+    TF1 func("func","gaus(0)+expo(3)");
+    func.SetParLimits(0,0,1.e9);
+    func.SetParLimits(1,-1.5,0.);
+    func.SetParLimits(2,0.5,2.);
+    func.SetParameter(1,-0.7);
+    func.SetParameter(2,0.7);
+    func.SetParLimits(4,-20.,10.);
+    if (kPtBins[iB-1]<0.5){for (int i=0;i<2;++i)fNSigmaITSpT->Fit("func","RQL","",-3.5,0.2);}
+    else if (kPtBins[iB-1]<0.6){for (int i=0;i<2;++i)fNSigmaITSpT->Fit("func","RQL","",-3.,0.2);}
+    else{for (int i=0;i<2;++i)fNSigmaITSpT->Fit("func","RQL","",-2.5,0.2);}
+    //if (fNSigmaITSpT->GetFunction("func")->GetChisquare()/fNSigmaITSpT->GetFunction("func")->GetNDF() > 3.) continue;
+    fITSCalibMean.SetBinContent(iB,fNSigmaITSpT->GetFunction("func")->GetParameter(1));
+    fITSCalibMean.SetBinError(iB,fNSigmaITSpT->GetFunction("func")->GetParError(1));
+    fITSCalibSigma.SetBinContent(iB,fNSigmaITSpT->GetFunction("func")->GetParameter(2));
+    fITSCalibSigma.SetBinError(iB,fNSigmaITSpT->GetFunction("func")->GetParError(2));
+    fITSCalibMeanRoo.SetBinContent(iB,mu.getVal());
+    fITSCalibMeanRoo.SetBinError(iB,mu.getError());
+    fITSCalibSigmaRoo.SetBinContent(iB,sig.getVal());
+    fITSCalibSigmaRoo.SetBinError(iB,sig.getError());
+    fNSigmaITSpT->Write();
+    xframe->Write();
   }
+  outFile.cd("");
+  fITSCalibMean.Write();
+  fITSCalibSigma.Write();
+  fITSCalibMeanRoo.Write();
+  fITSCalibSigmaRoo.Write();
 }
