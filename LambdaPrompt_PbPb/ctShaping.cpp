9c6fe4e96130475f0f7602343fc71dbe2583d7dd
maciacco <mariociacco97@gmail.com>
2022-08-22 11:05:19 +0200

    use reweighting in ct and pt shaping

--- LambdaPrompt_PbPb/ctShaping.cpp
+++ LambdaPrompt_PbPb/ctShaping.cpp
@@ -9,13 +9,15 @@
 #include <ROOT/RDataFrame.hxx>
 #include "AliPWGFunc.h"
 
-const char* kInFileMCName = "../data/LambdaPrompt_PbPb/AnalysisResults_testCtMotherMC.root";
+const char* kInFileMCName = "./AnalysisResults_reweight_BW_30_50.root";
 const char* kInFileCentName = "../data/LambdaPrompt_PbPb/StrangenessRatios_summary.root";
+
+constexpr bool reject = false;
 
 int iPart = 1;
 double speed_of_light = 2.99792458;
 double xi_0_lifetime = 2.90;
-double max_ct = 20;
+double max_ct = 100;
 
 int int_pow(int a, int b){
   if (b < 0) return -999;
@@ -39,25 +41,38 @@ void ctShaping(const char *inFileMCName=kInFileMCName, const char *inFileCentNam
   fExp.SetParameter(1,1/xi_0_lifetime/speed_of_light);
   auto hNorm = df.Filter(Form("(flag & BIT(%d))==%d",iPart,part_flag)).Histo1D({"hNorm","hNorm",20,0,max_ct},cut_variable.data());
   hNorm->GetXaxis()->SetRangeUser(5,max_ct);
-  double normMin = 1; //hNorm->GetMinimum();
+  double normMin = hNorm->GetMinimum();
   hNorm->GetXaxis()->SetRangeUser(0.,max_ct);
   TF1 fExpNorm("fExpNorm","[0]*exp(-[1]*x)",0,max_ct);
   TH1D *hTmp = new TH1D(*hNorm);
-  hTmp->Fit("fExpNorm","MRL+","",0,20);
+  fExpNorm.SetParLimits(0,0.,1.e8);
+  fExpNorm.SetParLimits(1,0.,.5);
+  hTmp->Fit("fExpNorm","MR+","",0,max_ct);
   fExp.SetParameter(0,fExpNorm.Eval(max_ct)/TMath::Exp(-max_ct/xi_0_lifetime/speed_of_light));
-  auto reweight = [fExpNorm, normMin, fExp](float ct){
-    double hNormVal = fExpNorm.Eval(ct);
-    double exp = fExp.Eval(ct);
-    bool cut = (gRandom->Rndm()*hNormVal) < exp;
-    return cut;
-  };
   std::cout << "Process tree (2)..." << std::endl;
   std::cout << "cut variable = " << cut_variable.data() << "; iPart = " << iPart << "; part_flag = " << part_flag << std::endl;
-  df.Filter(Form("(flag & BIT(%d))==%d",iPart,part_flag)).Filter(reweight,{cut_variable.data()}).Snapshot("LambdaTree",Form("AnalysisResults_%d.root",iPart));
+  if (reject){
+    auto reweight = [fExpNorm, normMin, fExp](float ct){
+      double hNormVal = fExpNorm.Eval(ct);
+      double exp = fExp.Eval(ct);
+      bool cut = (gRandom->Rndm()*hNormVal) < exp;
+      return cut;
+    };
+    df.Filter(Form("(flag & BIT(%d))==%d && %s < %f",iPart,part_flag,cut_variable.data(),max_ct)).Filter(reweight,{cut_variable.data()}).Snapshot("LambdaTree",Form("AnalysisResults_%d.root",iPart));
+  }
+  else{
+    auto reweight = [fExpNorm, normMin, fExp](float ct){
+      double hNormVal = fExpNorm.Eval(ct);
+      double exp = fExp.Eval(ct);
+      double weight = exp/hNormVal;
+      return weight;
+    };
+    df.Filter(Form("(flag & BIT(%d))==%d && %s < %f",iPart,part_flag,cut_variable.data(),max_ct)).Define("weightMC",reweight,{cut_variable.data()}).Snapshot("LambdaTree",Form("AnalysisResults_%d.root",iPart));
+  }
   TCanvas canv("c","c");
-  // canv.SetLogy();
-  // fExpNorm.Draw();
-  // fExp.Draw("same");
-  // canv.Print("c.png");
-  gSystem->Exec("mv AnalysisResults_1.root AnalysisResults_Xi0_reweight_BW_0_90.root");
+  canv.SetLogy();
+  fExpNorm.Draw();
+  fExp.Draw("same");
+  canv.Print("c.png");
+  gSystem->Exec("mv AnalysisResults_1.root AnalysisResults_Xi0_reweight_BW_30_50.root");
 }
--- LambdaPrompt_PbPb/pTShaping.cpp
+++ LambdaPrompt_PbPb/pTShaping.cpp
@@ -19,6 +19,12 @@ constexpr double T_kin[nCent] = {0.090, 0.091, 0.094, 0.097, 0.101, 0.108, 0.115
 constexpr double n[nCent] = {0.735, 0.736, 0.739, 0.771, 0.828, 0.908, 1.052, 1.262, 1.678, 2.423};
 constexpr double avg_mult[nCent] = {1943, 1587, 1180, 786, 512, 318, 183, 96.3, 44.9, 17.5};
 
+constexpr int centSplit[][2]={{0,0},{1,1},{4,5},{0,10}}; // 0-5%, 5-10%, 30-50%, 0-90%
+constexpr int nCentSplit[]={1,1,2,10}; // 0-5%, 5-10%, 30-50%, 0-90%
+constexpr int centClass = 0; // 0-5% -> 0, 5-10% -> 1, 30-50% -> 2, 0-90% -> 3
+
+constexpr bool reject = true;
+
 double beta_max(const double beta_avg, const double n){
   return beta_avg*0.5*(2+n);
 }
@@ -32,7 +38,7 @@ int int_pow(int a, int b){
 }
 
 double full_bw(TF1 *bw_array[], double pT){
-  int sz = nCent;
+  int sz = nCentSplit[centClass];
   double bw = 0;
   for (int iF=0; iF<sz; ++iF){
     bw+=bw_array[iF]->Eval(pT);
@@ -48,9 +54,10 @@ double full_bw_max(TF1 *bw_array[], double max_steps = 1.e3, double max_pT = 10.
   return h.GetMaximum();
 }
 
-const char* kInFileMCName = "../data/LambdaPrompt_PbPb/AnalysisResults_LHC21l5_lambdaMC_child123.root";
+const char* kInFileMCName = "../data/LambdaPrompt_PbPb/AnalysisResults_LHC21l5_LambdaCtMotherAndProtonsITSPID.root";
 const char* kInFileCentName = "../data/LambdaPrompt_PbPb/StrangenessRatios_summary.root";
-const char* kOutFileName = "pTShapesLXiOm.root";
+const char* kOutFileName = "pTShapesLXiOm_0_5.root";
+
 
 void pTShaping(const char *inFileMCName=kInFileMCName, const char *inFileCentName=kInFileCentName, const char *outFileName=kOutFileName){
   ROOT::EnableImplicitMT(4);
@@ -58,7 +65,7 @@ void pTShaping(const char *inFileMCName=kInFileMCName, const char *inFileCentNam
   TFile outFile(outFileName, "recreate");
   ROOT::RDataFrame df("LambdaTree",inFileMCName);
   TH1D *hCent = (TH1D*)inFileCent.Get("Centrality_selected");
-  double nEv = hCent->Integral();
+  double nEv = hCent->Integral(cent[centSplit[centClass][0]]+1,cent[centSplit[centClass][1]+1]);
   AliPWGFunc *pwgFunc = new AliPWGFunc();
   for (int iPart = 0; iPart < 3; ++iPart){
     int int_pow_part = int_pow(2,iPart);
@@ -67,16 +74,16 @@ void pTShaping(const char *inFileMCName=kInFileMCName, const char *inFileCentNam
     TString dir(Form("bw_%s",partNames[iPart]));
     outFile.mkdir(dir);
     outFile.cd(dir);
-    TF1 *blastWave[nCent];
-    std::string name[nCent];
-    for (int iC = 0; iC < nCent; ++iC){
+    TF1 *blastWave[nCentSplit[centClass]];
+    std::string name[nCentSplit[centClass]];
+    for (int iC = centSplit[centClass][0]; iC <= centSplit[centClass][1]; ++iC){
       double nEvInRange = hCent->Integral(cent[iC]+1,cent[iC+1]);
-      name[iC] = "bw_";
-      name[iC] += std::to_string(iC);
-      blastWave[iC] = pwgFunc->GetBGBW(kMass[iPart],beta_max(beta_avg[iC],n[iC]),T_kin[iC],n[iC],1,name[iC].data());
-      blastWave[iC] = pwgFunc->GetBGBW(kMass[iPart],beta_max(beta_avg[iC],n[iC]),T_kin[iC],n[iC],avg_mult[iC]*nEvInRange/nEv/blastWave[iC]->Integral(0,1.e3),name[iC].data());
+      name[iC-centSplit[centClass][0]] = "bw_";
+      name[iC-centSplit[centClass][0]] += std::to_string(iC);
+      blastWave[iC-centSplit[centClass][0]] = pwgFunc->GetBGBW(kMass[iPart],beta_max(beta_avg[iC],n[iC]),T_kin[iC],n[iC],1,name[iC-centSplit[centClass][0]].data());
+      blastWave[iC-centSplit[centClass][0]] = pwgFunc->GetBGBW(kMass[iPart],beta_max(beta_avg[iC],n[iC]),T_kin[iC],n[iC],avg_mult[iC]*nEvInRange/nEv/blastWave[iC-centSplit[centClass][0]]->Integral(0,1.e3),name[iC-centSplit[centClass][0]].data());
       std::cout << "weight = " << nEvInRange/nEv << std::endl;
-      blastWave[iC]->Write();
+      blastWave[iC-centSplit[centClass][0]]->Write();
     }
     TH1D *hBW=new TH1D("h","h",100000,0,10);
     for (int iB=1;iB<=100000;++iB){
@@ -95,18 +102,30 @@ void pTShaping(const char *inFileMCName=kInFileMCName, const char *inFileCentNam
     double normMin = hNorm->GetMinimum();
     hNorm->GetXaxis()->SetRangeUser(0.,10.);
     TH1D *hTmp = new TH1D(*hNorm);
-    auto reweight = [hTmp, normMin, bw_max, hBW](float pT){
-      int sz = nCent;
-      double bw = hBW->GetBinContent(hBW->FindBin(pT));
-      double hNormVal = hTmp->GetBinContent(hTmp->FindBin(pT));
-      bool cut = (gRandom->Rndm()*hNormVal*bw_max) < (normMin*0.99*bw);
-      return cut;
-    };
     std::cout << "Process tree (2)..." << std::endl;
     std::cout << "cut variable = " << cut_variable.data() << "; iPart = " << iPart << "; int_pow_part = " << int_pow_part << std::endl;
-    dff.Filter(Form("(flag & BIT(%d))==%d",iPart,int_pow_part)).Filter(reweight,{cut_variable.data()}).Snapshot("LambdaTree",Form("AnalysisResults_%d.root",iPart));
+    if (reject){
+      auto reweight = [hTmp, normMin, bw_max, hBW](float pT){
+        int sz = nCentSplit[centClass];
+        double bw = hBW->GetBinContent(hBW->FindBin(pT));
+        double hNormVal = hTmp->GetBinContent(hTmp->FindBin(pT));
+        bool cut = gRandom->Rndm() < (bw*normMin/hNormVal/bw_max);
+        return cut;
+      };
+      dff.Filter(Form("(flag & BIT(%d))==%d",iPart,int_pow_part)).Filter(reweight,{cut_variable.data()}).Snapshot("LambdaTree",Form("AnalysisResults_%d.root",iPart));
+    }
+    else {
+      auto reweight = [hTmp, normMin, bw_max, hBW](float pT){
+        int sz = nCentSplit[centClass];
+        double bw = hBW->GetBinContent(hBW->FindBin(pT));
+        double hNormVal = hTmp->GetBinContent(hTmp->FindBin(pT));
+        double weight=bw*normMin/hNormVal/bw_max;
+        return weight;
+      };
+      dff.Filter(Form("(flag & BIT(%d))==%d",iPart,int_pow_part)).Define("weightMC",reweight,{cut_variable.data()}).Snapshot("LambdaTree",Form("AnalysisResults_%d.root",iPart));
+    }
   }
-  gSystem->Exec("alihadd AnalysisResults_reweight_BW_0_90.root AnalysisResults_0.root AnalysisResults_1.root AnalysisResults_2.root");
+  gSystem->Exec("alihadd AnalysisResults_reweight_BW_0_5.root AnalysisResults_0.root AnalysisResults_1.root AnalysisResults_2.root");
   gSystem->Exec("rm AnalysisResults_0.root AnalysisResults_1.root AnalysisResults_2.root");
   outFile.Close();
 }
